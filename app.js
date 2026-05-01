9572c2b11933073ccae215d883b01648047999c9
iker <iker.hiram.ramirez@gmail.com>
2026-05-01 09:12:30 -0600

    app.js 1.1

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const paddleLeft = document.querySelector(".caja-container .caja");
 const paddleRight = document.querySelector("#caja_dos .caja");
 
 const game = document.querySelector(".game");
-const ball = document.querySelector(".ball");
-
-let ballX;
-let ballY;
 
 let speedX = 3;
 let speedY = 3;
@@ -42,6 +38,23 @@ function update() {
     if (ballX <= 0 || ballX >= gameWidth - ballSize) {
         speedX *= -1;
     }
+
+    const ballRect = ball.getBoundingClientRect();
+    const leftRect = paddleLeft.getBoundingClientRect();
+    const rightRect = paddleRight.getBoundingClientRect();
+
+    if (isColliding(ballRect, leftRect)) {
+    ballX = leftRect.right;
+    speedX = Math.abs(speedX);
+}
+
+    if (isColliding(ballRect, rightRect)) {
+    ballX = rightRect.left - 20;
+    speedX = -Math.abs(speedX);
+}
+
+console.log(isColliding(ballRect, leftRect));
+
 }
 
  function render() {
@@ -49,3 +62,25 @@ function update() {
 }
 
 console.log(ballX, ballY);
+
+function isColliding(a, b) {
+    return (
+        a.left < b.right &&
+        a.right > b.left &&
+        a.top < b.bottom &&
+        a.bottom > b.top
+    );
+}
+
+if (isColliding(ballRect, leftRect)) {
+    ballX = leftRect.right;
+    speedX = Math.abs(speedX);
+}
+
+if (isColliding(ballRect, rightRect)) {
+    ballX = rightRect.left - 20;
+    speedX = -Math.abs(speedX);
+}
+
+const hitPoint = (ballRect.top - leftRect.top) / leftRect.height;
+speedY = (hitPoint - 0.5) * 10;
